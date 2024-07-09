8529a6c72a8788492514166867083c4d92c27fc4
masab2005 <muhammad.masab05@gmail.com>
2024-07-09 07:29:18 +0000

    included jobs

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask , render_template
 app = Flask(__name__)
+JOBS = [
+    {
+    'name':'masab',
+    'age': 19,
+
+    }
+]
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS)
 if __name__ == '__main__':
      app.run(host='0.0.0.0', debug=True)
